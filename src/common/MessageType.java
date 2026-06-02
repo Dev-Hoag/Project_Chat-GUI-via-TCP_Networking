890ac491a6acdf7742342ab16ec1355180955cba
TrungHieu <hoangtrunghieu11072002@gmail.com>
2026-06-02 16:03:37 +0700

    Remove project from repository

--- src/common/MessageType.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public final class MessageType {
-    public static final String TEXT = "TEXT";
-    public static final String FILE = "FILE";
-
-    private MessageType() {
-    }
-}
